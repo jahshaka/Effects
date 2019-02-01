d3abb28d3a6d60c2950e6e0e54308fa5012df485
unknown <willroymyles@yahoo.com>
2019-02-01 14:59:28 -0600

    Started copying over image to project

--- shaderassetwidget.cpp
+++ shaderassetwidget.cpp
@@ -9,8 +9,11 @@
 
 #include "irisgl/src/core/irisutils.h"
 #include "core/materialhelper.h"
+#include "properties.h"
+#include <QStandardPaths>
+#include <QDirIterator>
 
-
+#include "shadergraphmainwindow.h"
 #if(EFFECT_BUILD_AS_LIB)
 #include "../io/assetmanager.h"
 #include "../uimanager.h"
@@ -266,15 +269,8 @@ void ShaderAssetWidget::createShader(QListWidgetItem * item)
 	const QString newShader = "Untitled Shader";
 	
 	item->setSizeHint(currentSize);
-	
-	auto genGuid = []() {
-		auto id = QUuid::createUuid();
-		auto guid = id.toString().remove(0, 1);
-		guid.chop(1);
-		return guid;
-	};
 
-	const QString targetGuid = genGuid();
+	const QString targetGuid = shadergraph::MainWindow::genGUID();
 
 	
 	assetItemShader.wItem = item;
@@ -285,10 +281,48 @@ void ShaderAssetWidget::createShader(QListWidgetItem * item)
 	AssetRecord sourceRecord = db->fetchAsset(sourceGuid);
 	auto sourceData = db->fetchAssetData(sourceGuid);
 
+	auto doc = QJsonDocument::fromBinaryData(sourceData);
+	auto obj = doc.object();
+	auto list = obj["properties"].toArray();
+
+
+		for (auto prop : list) {
+			auto type = prop.toObject()["type"].toString();
+			if (type == "texture") {
+				auto value = prop.toObject()["value"].toString();
+
+				auto assetPath = IrisUtils::join(
+					QStandardPaths::writableLocation(QStandardPaths::DataLocation),
+					"AssetStore"
+				);
+				
+
+				const QString assetFolder = QDir(assetPath).filePath(value);
+				QDirIterator it(assetFolder);
+
+				if (it.hasNext()) {
+					auto imgGuid = shadergraph::MainWindow::genGUID();
+					QFile file(it.next());
+					QImage img(file.fileName);
+					db->createAssetEntry(targetGuid, imgGuid, file.fileName(), static_cast<int>(ModelTypes::Texture));
+
+
+					
+				}
+
+
+
+				qDebug() << value;
+			}
+		}
+	
+
 	db->createAssetEntry(targetGuid,
 		sourceRecord.name,
 		static_cast<int>(ModelTypes::Shader),
 		assetItemShader.selectedGuid);
+
+
 	db->updateAssetAsset(targetGuid, sourceData);
 	db->updateAssetThumbnail(targetGuid, db->fetchAsset(item->data(MODEL_GUID_ROLE).toString()).thumbnail);
 
